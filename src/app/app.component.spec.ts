ebe6285cc3038921450bb3958a52e47730738552
YorchOfTheCity <31016218+YorchOfTheCity@users.noreply.github.com>
2017-08-23 10:14:15 -0700

    Done with login screen html, added tests for its form validation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,32 +1,88 @@
 import { TestBed, async } from '@angular/core/testing';
-
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ComponentFixture, fakeAsync } from '@angular/core/testing';
+
+
+let comp: AppComponent;
+let fixture: ComponentFixture<AppComponent>;
+let debugInstance;
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        ReactiveFormsModule
+      ],
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+    }).compileComponents().then(
+      () => {
+        fixture = TestBed.createComponent(AppComponent);
+        comp = fixture.componentInstance;
+        debugInstance = fixture.debugElement.componentInstance;
+      }
+      );
   }));
 
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(debugInstance).toBeTruthy();
+    expect(comp).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+  it(`should reject incomplete forms`, async(() => {
+
+    comp.formModel.controls['username'].setValue('');
+    comp.formModel.controls['password'].setValue('');
+
+    const usernameInput = comp.formModel.get('username');
+    const pwdInput = comp.formModel.get('password');
+
+    expect(comp.formModel.invalid).toBeTruthy();
+    expect(usernameInput.invalid).toBeTruthy();
+    expect(pwdInput.invalid).toBeTruthy();
   }));
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+  it('form value should update from form changes', fakeAsync(() => {
+    comp.formModel.controls['username'].setValue('testUser');
+    comp.formModel.controls['password'].setValue('testPwd');
+
+    expect(comp.formModel.value).toEqual({username: 'testUser', password: 'testPwd'});
+  }));
+
+  it('should accept a form with both fields filled', fakeAsync(() => {
+    const usernameInput = comp.formModel.get('username');
+    const pwdInput = comp.formModel.get('password');
+
+    expect(comp.formModel.valid).toBeFalsy();
+    expect(usernameInput.valid).toBeFalsy();
+    expect(pwdInput.valid).toBeFalsy();
+
+    comp.formModel.controls['username'].setValue('testUser');
+    expect(comp.formModel.valid).toBeFalsy();
+    expect(usernameInput.valid).toBeTruthy();
+    expect(pwdInput.valid).toBeFalsy();
+
+    comp.formModel.controls['username'].setValue('');
+    comp.formModel.controls['password'].setValue('testPwd');
+    expect(comp.formModel.valid).toBeFalsy();
+    expect(usernameInput.valid).toBeFalsy();
+    expect(pwdInput.valid).toBeTruthy();
+
+    comp.formModel.controls['username'].setValue('testUser');
+    expect(comp.formModel.valid).toBeTruthy();
+    expect(usernameInput.valid).toBeTruthy();
+    expect(pwdInput.valid).toBeTruthy();
+  }));
+
+  xit('should reject a wrong user/pwd combination', fakeAsync(() => {
+    console.log('TODO');
+  }));
+
+  xit('should get to the chat page when users/pwd are correct', fakeAsync(() => {
+    console.log('TODO');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 declare var Vivus: any;
-import { Component, OnInit } from '@angular/core';
 import 'jquery';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,23 @@ import 'jquery';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'app';
+  formModel: FormGroup;
+  submitted = false;
+
+  constructor() {
+    this.formModel = new FormGroup({
+      'username': new FormControl('', [Validators.required]),
+      'password': new FormControl('', [Validators.required])
+      });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(`submitted is now: ${this.submitted}`);
+    console.dir(this.formModel.value);
+    console.log(`username is valid: ${this.formModel.get('username').valid}`);
+    console.log(`username with get is valid: ${this.username.valid}`);
+  }
 
   ngOnInit(): void {
     const useless: any = new Vivus('title-svg', { duration: 100 }, this.fillSVG);
@@ -17,20 +34,20 @@ export class AppComponent implements OnInit {
   fillSVG(): void {
     const transparency = .1;
     const timeout = 20;
-    // document.getElementById('title-cn').setAttribute('class', 'fillSVGBlack');
-    // document.getElementById('title-cn').className += 'fillSVGBlack';
 
-    // document.getElementById('title-cn').style.fill = 'rgba(10,10,10,.1)';
-    // document.getElementById('title-hat-oir').style.fill = 'rgba(253,20,20,.1)';
     // Can't seem to be able to loop neither with css transitions nor jquery's animate, will do it by hand.
-    (function loop(trans){
+    (function loop(trans) {
       document.getElementById('title-cn').style.fill = 'rgba(10,10,10,' + trans + ')';
       document.getElementById('title-hat-oir').style.fill = 'rgba(253,20,20,' + trans + ')';
       if (trans < 1) {
-        setTimeout(function() {
+        setTimeout(function () {
           loop(trans + .1);
         }, timeout);
       }
     })(transparency);
   }
+
+  // Getters for easier validator syntax in template:
+  get username() {return this.formModel.get('username'); }
+  get password() {return this.formModel.get('password'); }
 }
